7d687e2baebef3f3a145189235dc579048d3af57
Justin Tulloss <justin.tulloss@gmail.com>
2016-09-08 16:15:04 -0700

    Wrote the app

--- Gemfile
+++ Gemfile
@@ -1,2 +1,6 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+gem 'thin'
+gem 'rerun'
+gem 'twilio-ruby'
+gem 'pony'
---
+++ app.rb
@@ -0,0 +1,48 @@
+require 'pony'
+require 'sinatra'
+require 'twilio-ruby'
+
+GREETING = ENV['PHONETAG_GREETING'] || 'Please leave a message after the beep'
+EMAIL = ENV['PHONETAG_EMAIL']
+TWILIO_SID = ENV['PHONETAG_TWILIO_SID']
+TWILIO_TOKEN = ENV['PHONETAG_TWILIO_TOKEN']
+
+twilio_client = Twilio::REST::Client.new TWILIO_SID, TWILIO_TOKEN
+
+
+get '/' do
+  'Welcome to phone tag'
+end
+
+post '/call' do
+  Twilio::TwiML::Response.new do |r|
+    r.Say GREETING
+    r.Record(
+      :maxlength => '60',
+      :action => '/recording',
+      :transcribeCallback => '/transcription',
+    )
+  end.text
+end
+
+post '/recording' do
+  logger.info "Got a new recording, listen at #{params['RecordingUrl']}.mp3"
+  status 201
+end
+
+post '/transcription' do
+  recording = twilio_client.recordings.get(params['RecordingSid'])
+  full_text = recording.transcriptions.list().reduce('') do |text, transcription|
+    text += transcription.transcription_text
+  end
+  email_body = erb(:email, :locals => {
+    :text => full_text,
+    :from => params[:From],
+    :recording => recording
+  })
+  Pony.mail :to => EMAIL,
+            :from => "no-reply@#{request.host}",
+            :subject => "New Phone Call from #{params[:From]}",
+            :body => email_body
+  status 201
+end
